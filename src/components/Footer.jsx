9982779870851e90d41683fbcbf328b4926a90c6
Nịnh Thanh Khương <165678100+maiychrus25@users.noreply.github.com>
2025-04-05 08:07:46 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="border-t-[1px] border-solid flex justify-center items-center mt-auto w-full">
       <h5 className="text-slate-600 text-[12px] font-normal pb-0 tracking-[0.88px] mb-0 mt-6 text-center">
-        Copyright © 2020 NỊNH THANH KHƯƠNG - Yurki TIKO
+        Copyright © 2025 NỊNH THANH KHƯƠNG - Yurki TIKO
       </h5>
     </div>
   );
